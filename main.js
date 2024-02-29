7b428be0176b1a058d16a3340caa249b1833304a
khumoyunhusanov <rashidovalutfiya@gmail.com>
2024-02-29 15:13:49 +0500

    console

---
+++ main.js
@@ -0,0 +1,74 @@
+
+// 1
+
+// let a = prompt("son yozing")
+// let b = prompt("ikkinchi sonni yozing")
+
+// alert(parseInt(a) + parseInt(b))
+
+// // 2
+
+
+// let f = +prompt("biron bir sonni tanlang")
+// let h = +prompt("va uni kupaytiring")
+
+
+// console.log(f*h);
+ 
+
+// // 3
+
+
+// let j = +prompt("son tanlang")
+// let g = +prompt("tanlagan soningizdi buling")
+
+// console.log(j/g);
+
+
+// // 4
+
+// let m = +prompt("sonlarni qushing")
+// let l = +prompt("sonlarni tanlang")
+
+// console.log(m+l);
+
+// let a = 15;
+
+// if (a < 50) {
+//     console.log("katta");
+// } else {
+//     console.log("kichik");
+// }
+ 
+//  let txt = "salom"
+
+// if () {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// let txt = "salom"
+// let a = txt.charAt(0)
+// if (txt.charAt===txt.charAt(0)) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+let a = +prompt("son yozing")
+let b = +prompt("yozgan soningizdi qushing")
+let e = +prompt("javob")
+
+let d = 
+
+
+
+if (d) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+ 
